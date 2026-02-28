49d05a8edab6216fdaac90c5f1b9c85b50cdc28c
Rumia Channel <github@rumia-ch.uk>
2026-02-28 23:37:18 +0900

    Add Windows compatibility for usleep function

--- sl.c
+++ sl.c
@@ -46,7 +46,12 @@
 
 #include <curses.h>
 #include <signal.h>
+#ifndef _WIN32
 #include <unistd.h>
+#else
+#include <windows.h>
+#define usleep(x) Sleep((x) / 1000)
+#endif
 #include "sl.h"
 
 void add_kininarimasu(int y, int x);
